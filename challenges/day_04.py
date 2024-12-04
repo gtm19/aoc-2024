16edf73ec3cbb47c0f0859b42fa30c263c52a57c
gtm19 <9137502+gtm19@users.noreply.github.com>
2024-12-04 14:36:02 -0400

    day 4 complete

--- challenges/day_04.py
+++ challenges/day_04.py
@@ -18,16 +18,34 @@ def count_xmas(lines: list[str]) -> int:
                 (-1, 1),
             ]:
                 try:
-                    maybe_mas = "".join(
-                        lines[try_y][try_x]
-                        for mult in (1, 2, 3)
-                        if len(lines) > (try_y := y + (y_offset * mult)) >= 0
-                        and len(lines[0]) > (try_x := x + (x_offset * mult)) >= 0
-                    )
-                    if maybe_mas == "MAS":
+                    if (
+                        "".join(
+                            lines[try_y][try_x]
+                            for mult in (1, 2, 3)
+                            if len(lines) > (try_y := y + (y_offset * mult)) >= 0
+                            and len(lines[0]) > (try_x := x + (x_offset * mult)) >= 0
+                        )
+                        == "MAS"
+                    ):
                         total += 1
                 except IndexError:
                     continue
+    return total
+
+
+def count_crosses(lines: list[str]) -> int:
+    total = 0
+    for x, y in product(range(1, len(lines) - 1), range(1, len(lines[0]) - 1)):
+        if lines[y][x] == "A":
+            try:
+                if (
+                    {lines[y - 1][x - 1], lines[y + 1][x + 1]}
+                    == {lines[y + 1][x - 1], lines[y - 1][x + 1]}
+                    == {"M", "S"}
+                ):
+                    total += 1
+            except IndexError:
+                continue
     return total
 
 
@@ -36,7 +54,7 @@ def part_1(lines: list[str]) -> int:
 
 
 def part_2(lines: list[str]) -> int:
-    pass
+    return count_crosses(lines)
 
 
 if __name__ == "__main__":
