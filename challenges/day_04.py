4b5d6ee083930636ded314c2b32fdbccd150bb6b
gtm19 <9137502+gtm19@users.noreply.github.com>
2024-12-04 21:21:09 -0400

    day 4 tidy up

--- challenges/day_04.py
+++ challenges/day_04.py
@@ -1,51 +1,46 @@
-from itertools import product
-
 from challenges.utils import todays_lines
 
 
 def count_xmas(lines: list[str]) -> int:
     total = 0
-    for x, y in product(range(len(lines)), range(len(lines[0]))):
-        if lines[y][x] == "X":
-            for x_offset, y_offset in [
-                (1, 0),
-                (0, 1),
-                (1, 1),
-                (-1, 0),
-                (0, -1),
-                (-1, -1),
-                (1, -1),
-                (-1, 1),
-            ]:
-                try:
-                    if (
-                        "".join(
-                            lines[try_y][try_x]
-                            for mult in (1, 2, 3)
-                            if len(lines) > (try_y := y + (y_offset * mult)) >= 0
-                            and len(lines[0]) > (try_x := x + (x_offset * mult)) >= 0
-                        )
-                        == "MAS"
-                    ):
+    for x, y in [
+        (x, y)
+        for y, row in enumerate(lines)
+        for x, char in enumerate(row)
+        if char == "X"
+    ]:
+        for x_offset, y_offset in [
+            (x_offset, y_offset)
+            for x_offset in range(-1, 2)
+            for y_offset in range(-1, 2)
+            if (x_offset, y_offset) != (0, 0)
+        ]:
+            for mult, letter in zip(range(1, 4), "MAS"):
+                if (
+                    len(lines) > (try_y := y + (y_offset * mult)) >= 0
+                    and len(lines[0]) > (try_x := x + (x_offset * mult)) >= 0
+                ):
+                    if lines[try_y][try_x] != letter:
+                        break
+                    if letter == "S":
                         total += 1
-                except IndexError:
-                    continue
     return total
 
 
 def count_crosses(lines: list[str]) -> int:
     total = 0
-    for x, y in product(range(1, len(lines) - 1), range(1, len(lines[0]) - 1)):
-        if lines[y][x] == "A":
-            try:
-                if (
-                    {lines[y - 1][x - 1], lines[y + 1][x + 1]}
-                    == {lines[y + 1][x - 1], lines[y - 1][x + 1]}
-                    == {"M", "S"}
-                ):
-                    total += 1
-            except IndexError:
-                continue
+    for x, y in [
+        (x, y)
+        for y, row in tuple(enumerate(lines))[1:-1]
+        for x, char in tuple(enumerate(row))[1:-1]
+        if char == "A"
+    ]:
+        if (
+            {lines[y - 1][x - 1], lines[y + 1][x + 1]}
+            == {lines[y + 1][x - 1], lines[y - 1][x + 1]}
+            == {"M", "S"}
+        ):
+            total += 1
     return total
 
 
