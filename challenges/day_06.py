586e07cceffbaaba93fa1a0d1baa6e40294cbcf9
Gareth Maddock <gtmaddock@gmail.com>
2024-12-07 17:58:55 -0400

    code is a bit nicer but might actually be slower

--- challenges/day_06.py
+++ challenges/day_06.py
@@ -1,73 +1,63 @@
-import re
 from itertools import cycle
+from time import perf_counter
 
 from challenges.utils import todays_lines
 
-DIRECTIONS = {
-    "^": (0, -1),
-    ">": (1, 0),
-    "v": (0, 1),
-    "<": (-1, 0),
-}
-
+DIRECTIONS = ((0, -1), (1, 0), (0, 1), (-1, 0))
 
 OBSTACLE = "#"
-START = "^"
+START = DIRECTIONS[0]
 
 # TODO: take another look at this where the obstacles are a list of locations
 # which you append to for part 2 - should be much quicker
 
 
-def inside_grid(loc, grid: list[str]) -> bool:
-    x, y = loc
-    if x < 0 or y < 0 or x > len(grid[0]) - 1 or y > len(grid) - 1:
-        return False
-    return True
+def make_grid(lines: list[str]) -> dict[tuple[int, int] : str]:
+    return {(x, y): c for y, row in enumerate(lines) for x, c in enumerate(row)}
 
 
-def walk_around(lines: list[str]) -> tuple[list, bool]:
-    DIRECTION_CYCLER = cycle(DIRECTIONS.keys())
-    visited = [
-        (START, (line.find(START), i)) for i, line in enumerate(lines) if START in line
-    ]
-    direction, (x, y) = visited[-1]
+def walk_around(grid, start) -> tuple[list, bool]:
+    direction_cycle = cycle(DIRECTIONS)
+
+    direction = next(direction_cycle)
+    x, y = start
+    visited = [(direction, (x, y))]
     while True:
-        next_x, next_y = x + DIRECTIONS[direction][0], y + DIRECTIONS[direction][1]
-        if not inside_grid((next_x, next_y), lines):
+        next_x, next_y = x + direction[0], y + direction[1]
+        next_value = grid.get((next_x, next_y))
+        if not next_value:
             infinite = False
             break
         if (direction, (next_x, next_y)) in visited:
             infinite = True
             break
-        next_value = lines[next_y][next_x]
-        if next_value == OBSTACLE:
-            direction = next(DIRECTION_CYCLER)
+        if next_value == "#":
+            direction = next(direction_cycle)
         else:
             visited.append((direction, (next_x, next_y)))
             x, y = next_x, next_y
     return visited, infinite
 
 
-def part_1(lines: list[str]) -> int:
-    visited = walk_around(lines)[0]
+def part_1(grid, start) -> int:
+    visited = walk_around(grid, start)[0]
     return len({loc[1] for loc in visited})
 
 
-def part_2(lines: list[str]) -> int:
-    visited = walk_around(lines)[0]
-    infinites = 0
-    checked = 0
-    for x, y in {loc[1] for loc in visited}:
-        checked += 1
-        if lines[y][x] == START:
-            continue
-        lines[y] = lines[y][:x] + OBSTACLE + lines[y][x + 1 :]
-        infinites += int(walk_around(lines)[1])
-        lines[y] = lines[y][:x] + "." + lines[y][x + 1 :]
-    return infinites
+def part_2(grid, start) -> int:
+    visited = walk_around(grid, start)[0]
+    return sum(
+        walk_around(grid | {obs: "#"}, start)[1]
+        for obs in {loc[1] for loc in visited}
+        if obs != start
+    )
 
 
 if __name__ == "__main__":
     input_lines = todays_lines(__file__, test=False)
-    print("PART 1: ", part_1(input_lines))
-    print("PART 2: ", part_2(input_lines))
+
+    grid = make_grid(input_lines)
+    start = [k for k, v in grid.items() if v == "^"][0]
+
+    print("PART 1: ", part_1(grid, start))
+    print("PART 2: ", part_2(grid, start))
