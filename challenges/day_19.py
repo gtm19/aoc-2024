262b74041abb57518ee98d3f2bd8b94b53c44c38
gtm19 <9137502+gtm19@users.noreply.github.com>
2024-12-19 20:12:22 -0400

    day 19 polishing

--- challenges/day_19.py
+++ challenges/day_19.py
@@ -5,29 +5,20 @@ from utils import DATA_DIR
 with open(DATA_DIR / "day_19.txt", "r") as f:
     lines = f.read()
 
-patterns, _, *designs = lines.strip().split("\n")
-patterns = patterns.split(", ")
+PATTERNS, _, *designs = lines.strip().split("\n")
 
 
 @cache
-def count_design_ways(target: str, parts: tuple[str]) -> int:
-    if target == "":
-        return 1
-
-    total = 0
-    for part in parts:
-        if target.startswith(part):
-            new_target = target[len(part) :]
-            total += count_design_ways(new_target, parts)
-
-    return total
+def count_design_ways(target: str) -> int:
+    return int(target == "") or sum(
+        count_design_ways(target.removeprefix(part))
+        for part in PATTERNS.split(", ")
+        if target.startswith(part)
+    )
 
 
-n_ways = tuple(count_design_ways(design, tuple(patterns)) for design in designs)
-p1 = sum(count > 0 for count in n_ways)
+n_ways = tuple(count_design_ways(design) for design in designs)
 
-print("Part 1: ", p1)
+print("Part 1: ", sum(map(bool, n_ways)))
 
-p2 = sum(n_ways)
-
-print("Part 2: ", p2)
+print("Part 2: ", sum(n_ways))
